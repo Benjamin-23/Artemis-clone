64bffed3225354b4cf17245985d393cbbff92116
Benjamin-23 <kitongabenja34@gmail.com>
2022-07-18 16:20:08 +0300

    add alerts and notification to store

---
+++ src/components/TheAlert.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { Notification, NotificationGroup } from "@progress/kendo-vue-notification";
+import { Fade } from "@progress/kendo-vue-animation";
+</script>
+
+<template>
+  <div class="z-10">
+    <NotificationGroup
+      style="right: 10px; buttom: 10px; flexwrap: wrap-reverse; alignitems: flex-start"
+    >
+      <Fade appear>
+        <Notification
+          :key="'success'"
+          :type="{ style: 'success', icon: true }"
+          :closable="true"
+          @close="onClose('success')"
+        >
+          <span> Your have saved you Data</span>
+        </Notification>
+      </Fade>
+    </NotificationGroup>
+  </div>
+</template>
+<style scoped></style>
---
+++ src/store/alert.ts
@@ -0,0 +1,7 @@
+import { defineStore } from 'pinia'
+
+export const alert = defineStore('alert'{ 
+    state: () => ({
+        alert:[]
+    })
+})
